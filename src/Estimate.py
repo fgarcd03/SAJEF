02b14e562474fe1891795a3ae3133f83c7b85b6b
Fernando García Diez <fer@fer-pc>
2020-11-21 22:21:31 +0100

    Añadido mensaje de error para cuando se elijan los mismo equipos y avance en la creación de los "equipos titulares"

--- src/Estimate.py
+++ src/Estimate.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import Conexion #archivo de la conexión con Neo4j para hacer consultas
 
 class Estimate:
     
-    def __init__(self):
-        pass #constructor vacío
+    def __init__(self,conexion,team1,team2):
+        self.Conexion.conexion = Conexion.conexion
+        self.team1 = team1
+        self.team2 = team2
+
         
-    def crearEquiposTitulares(equipo1, equipo2,jugadores1,jugadores2):#la lista de jugadores incluye en las posiciones pares los propios jugadores y las posiciones impares la posición en la que ocupan en el equipo
-        pass
+        self.createMainTeam(self.conexion,self.team1,self.team2)
+        
+        
+    def createMainTeam(self,conexion,team1,team2):#la lista de jugadores incluye en las posiciones pares los propios jugadores y las posiciones impares la posición en la que ocupan en el equipo
+        a = conexion.query("MATCH (p)-[r:PLAYS]->(c) WHERE c.id='{team}' RETURN DISTINCT p.name,r.teamPosition".format(team=team1))
+        print(a)
--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,48 +1,51 @@
 import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QComboBox,QPushButton,QGridLayout,QWidget,QLabel
+from PyQt5.QtWidgets import QComboBox,QPushButton,QGridLayout,QWidget,QLabel,QErrorMessage
 from PyQt5.QtCore import QSize
 from PyQt5.QtGui import QPainter,QPixmap
 
 import Conexion #archivo de la conexión con Neo4j para hacer consultas
-
+import Estimate #archivo donde va el algotirmo de calculo
 
 class MainWindow(QWidget):#QMainWindow
     
-    def __init__(self,teams):
+    def __init__(self,teams,conexion):#le pasamos la conexión al constructor para que lo pueda usar la clase estimate
         QWidget.__init__(self)#QMainWindow
+        
+        self.teams = teams
+        self.conexion = conexion
         
         self.grid_layout = QGridLayout(self)
         self.setLayout(self.grid_layout)
         self.setFixedSize(QSize(600, 400))    
         self.setWindowTitle("SAJEF") 
-        self.teams = teams
         
         self.team1 = QComboBox(self)
         self.team2 = QComboBox(self)
         self.elegir1 = QLabel("Elija el primer equipo")
         self.elegir2 = QLabel("Elija el segundo equipo")
-        self.errorEquipo = QLabel("Ha elegido el mismo equipo")
-        self.errorEquipo.hide() #La ocultamos, solo aparecera para mostrar error
         self.acept = QPushButton("Aceptar",self)
         self.acept.clicked.connect(self.accept_button) #evento para manejar el click del boton aceptar
         
         self.grid_layout.addWidget(self.elegir1,0,0)
-        self.grid_layout.addWidget(self.errorEquipo,0,1)
         self.grid_layout.addWidget(self.elegir2,0,2)
         self.grid_layout.addWidget(self.team1,1,0)
         self.grid_layout.addWidget(self.acept,1,1)
         self.grid_layout.addWidget(self.team2,1,2)
 
+
+        self.errorTeam = QErrorMessage()
+        self.errorTeam.setFixedSize(250,150)
+        
         for item in teams:
             self.team1.addItem(item)     
             self.team2.addItem(item)
 
     def accept_button(self):
         if str(self.team1.currentText()) == str(self.team2.currentText()):#si los dos equipos son el mismo,mostramos un error
-            self.errorEquipo.show()
-        else:
-            pass
+            self.errorTeam.showMessage("No puedes elegir el mismo equipo.")
+        else:#si pulsa el boton de aceptar y es correcto, primero tenemos que obtener de la base de datos los jugadores de los dos equipos y tambien las posiciones en las que juegan
+            Estimate.Estimate(self.conexion,str(self.team1.currentText()),str(self.team2.currentText())) #creamos el nuevo objeto y ya se encarga de llamar a todos los métodos el solo
             
         
     #Para poner el background sobrescribimos el QWidget
@@ -56,12 +59,13 @@ class MainWindow(QWidget):#QMainWindow
         
 if __name__ == "__main__":
     #Conexión y consulta
-    conexion = Conexion.Neo4j("bolt://localhost:11006", "neo4j", "SIBI20")
+    conexion = Conexion.Neo4j("bolt://localhost:7687", "neo4j", "SIBI20")
     teams = conexion.query("MATCH (p)-[r:PLAYS]->(c) RETURN DISTINCT c.id")
-    conexion.close()
     
     #Ventana
     app = QtWidgets.QApplication(sys.argv)
-    mainWin = MainWindow(teams)
+    mainWin = MainWindow(teams,conexion)
     mainWin.show()
     sys.exit( app.exec_() )
+    
+    conexion.close() #igual hay que moverlo
