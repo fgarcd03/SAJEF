925c396705449149afb1af22aac1b774e55b6c0e
fgarcd03 <fgarcd03@estudiantes.unileon.es>
2020-11-24 00:18:23 +0100

    Completada la creación de los equipos para la comparación

--- src/Estimate.py
+++ src/Estimate.py
@@ -14,16 +14,30 @@ class Estimate:
         
     def createMainTeam(self,conexion,team1,team2):#Táctica 4-3-3
         mainTeam1 = maintTeam2 = []
-        GK = LB = RB = CM = LM = RM = ST = LW = RW = ""
-        CB = 0
         
         players1 = conexion.query("MATCH (p)-[r:PLAYS]->(c) WHERE c.id='{team}' RETURN DISTINCT p.name,p.overall,r.teamPosition".format(team=team1)) #obtenemos todos los jugadores y sus correspondientes posiciones en los equipos
         players2 = conexion.query("MATCH (p)-[r:PLAYS]->(c) WHERE c.id='{team}' RETURN DISTINCT p.name,p.overall,r.teamPosition".format(team=team2))
         players1 = [player.replace("'", "") for player in players1] #limpiamos de comillas la lista de strings
         players2 = [player.replace("'", "") for player in players2]
         
+        mainTeam1 = self.filterTeam(players1)
+        mainTeam2 = self.filterTeam(players2)
+        print(mainTeam1)
+        print("-------")
+        print(mainTeam2)
+        if len(mainTeam1) != 11:
+            print("ERROR DE TAMAÑO DEL EQUIPO 1")
+        if len(mainTeam2) != 11:
+            print("ERROR DE TAMAÑO DEL EQUIPO 2")
         
-        for player in players1:
+        
+        
+        
+    def filterTeam(self,players):
+        GK = LBaLWB = RBaRWB = CM = LCM = RCM = ST = LW = RW = CDM = CAM = CF = LCB = RCB = ""
+        mainTeam = []
+        
+        for player in players:
             player = self.removeLastOccur(self.removeLastOccur(player, " "), " ")#Llamamos dos veces para que se eliminen los dos espacios
             overall = player.split(',')[1] #cojemos el substring que almacena el overall del jugador
             pos = player.split(',')[-1] #cojemos el substring que almacena la posición del jugador
@@ -31,47 +45,50 @@ class Estimate:
             if pos == "GK":
                 if (len(GK) > 0 and overall > GK.split(',')[1]) or len(GK) == 0: #Si había algo guardado y el overall es mayor, lo guardamos en su lugar, y si no había nada pues lo guardamos sin más
                     GK = player
-            if pos == "LB":
-                if (len(LB) > 0 and overall > LB.split(',')[1]) or len(LB) == 0: #Si había algo guardado y el overall es mayor, lo guardamos en su lugar, y si no había nada pues lo guardamos sin más
-                    LB = player
-            if pos == "RB":
-                if (len(RB) > 0 and overall > RB.split(',')[1]) or len(RB) == 0: #Si había algo guardado y el overall es mayor, lo guardamos en su lugar, y si no había nada pues lo guardamos sin más
-                    RB = player
+            if pos == "LB" or pos == "LWB":
+                if (len(LBaLWB) > 0 and overall > LBaLWB.split(',')[1]) or len(LBaLWB) == 0:
+                    LBaLWB = player
+            if pos == "RB" or pos == "RWB":
+                if (len(RBaRWB) > 0 and overall > RBaRWB.split(',')[1]) or len(RBaRWB) == 0:
+                    RBaRWB = player
             if pos == "CM":
-                if (len(CM) > 0 and overall > CM.split(',')[1]) or len(CM) == 0: #Si había algo guardado y el overall es mayor, lo guardamos en su lugar, y si no había nada pues lo guardamos sin más
+                if (len(CM) > 0 and overall > CM.split(',')[1]) or len(CM) == 0:
                     CM = player
-            if pos == "LM":
-                if (len(LM) > 0 and overall > LM.split(',')[1]) or len(LM) == 0: #Si había algo guardado y el overall es mayor, lo guardamos en su lugar, y si no había nada pues lo guardamos sin más
-                    LM = player
-            if pos == "RM":
-                if (len(RM) > 0 and overall > RM.split(',')[1]) or len(RM) == 0: #Si había algo guardado y el overall es mayor, lo guardamos en su lugar, y si no había nada pues lo guardamos sin más
-                    RM = player
+            if pos == "LCM":
+                if (len(LCM) > 0 and overall > LCM.split(',')[1]) or len(LCM) == 0:
+                    LCM = player
+            if pos == "RCM":
+                if (len(RCM) > 0 and overall > RCM.split(',')[1]) or len(RCM) == 0:
+                    RCM = player
             if pos == "ST":
-                if (len(ST) > 0 and overall > ST.split(',')[1]) or len(ST) == 0: #Si había algo guardado y el overall es mayor, lo guardamos en su lugar, y si no había nada pues lo guardamos sin más
+                if (len(ST) > 0 and overall > ST.split(',')[1]) or len(ST) == 0:
                     ST = player
             if pos == "LW":
-                if (len(LW) > 0 and overall > LW.split(',')[1]) or len(LW) == 0: #Si había algo guardado y el overall es mayor, lo guardamos en su lugar, y si no había nada pues lo guardamos sin más
+                if (len(LW) > 0 and overall > LW.split(',')[1]) or len(LW) == 0:
                     LW = player
             if pos == "RW":
-                if (len(RW) > 0 and overall > RW.split(',')[1]) or len(RW) == 0: #Si había algo guardado y el overall es mayor, lo guardamos en su lugar, y si no había nada pues lo guardamos sin más
+                if (len(RW) > 0 and overall > RW.split(',')[1]) or len(RW) == 0:
                     RW = player
+            if pos == "CDM":
+                if (len(CDM) > 0 and overall > CDM.split(',')[1]) or len(CDM) == 0:
+                    CDM = player
+            if pos == "CAM":
+                if (len(CAM) > 0 and overall > CAM.split(',')[1]) or len(CAM) == 0:
+                    CAM = player
+            if pos == "CF":
+                if (len(CF) > 0 and overall > CF.split(',')[1]) or len(CF) == 0:
+                    CF = player
+            if pos == "LCB":
+                if (len(LCB) > 0 and overall > LCB.split(',')[1]) or len(LCB) == 0:
+                    LCB = player
+            if pos == "RCB":
+                if (len(RCB) > 0 and overall > RCB.split(',')[1]) or len(RCB) == 0:
+                    RCB = player
         
-        mainTeam1.extend([GK,LB,RB,CM,LM,RM,ST,LW,RW])
-        print(mainTeam1)
-
+        mainTeam.extend([GK,LCB,RCB,LBaLWB,RBaRWB,CDM,CM,LCM,RCM,CAM,CF,ST,LW,RW])
+        mainTeam = [i for i in mainTeam if i] #quitamos huecos vacíos de posiciones que no se usa
         
-        
-    
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        return mainTeam
     
     #Borra la última ocurrencia de un char
     def removeLastOccur(self,string, char):
--- src/MainWindow.py
+++ src/MainWindow.py
@@ -25,37 +25,20 @@ class MainWindow(QWidget):#QMainWindow
         self.team2 = QComboBox(self)
         self.elegir1 = QLabel("Elija el primer equipo")
         self.elegir2 = QLabel("Elija el segundo equipo")
-        #Añadidos para hacer hueco
-        """
-        self.elegir3 = QLabel("")
-        self.elegir4 = QLabel("")
-        self.elegir5 = QLabel("")
-        self.elegir6 = QLabel("")
-        """
         self.acept = QPushButton("Aceptar",self)
         self.acept.clicked.connect(self.accept_button) #evento para manejar el click del boton aceptar
         
         self.gridLayout.addWidget(self.elegir1,0,0)
         self.gridLayout.addWidget(self.elegir2,0,2)
-        """
-        self.gridLayout.addWidget(self.elegir3,2,0)
-        self.gridLayout.addWidget(self.elegir4,3,2)
-        self.gridLayout.addWidget(self.elegir5,4,0)
-        self.gridLayout.addWidget(self.elegir6,5,2)
-        """
         self.gridLayout.addWidget(self.team1,1,0)
         self.gridLayout.addWidget(self.acept,1,1)
         self.gridLayout.addWidget(self.team2,1,2)
 
-
-
-        #self.hbox = QHBoxLayout()
-        #self.gridLayout.addLayout(self.hbox,2,1)
+        #Parte de la imágen de abajo
         self.labelImage = QLabel(self)
         self.pixmap = QPixmap("../resources/ball-soccer-600x400.jpg")
         self.labelImage.setPixmap(self.pixmap)
         self.gridLayout.addWidget(self.labelImage,2, 0, 2, 0)
-        #self.hbox.addWidget(self.labelImage)
 
         self.errorTeam = QErrorMessage()
         self.errorTeam.setFixedSize(250,150)
@@ -72,19 +55,10 @@ class MainWindow(QWidget):#QMainWindow
         else:#si pulsa el boton de aceptar y es correcto, primero tenemos que obtener de la base de datos los jugadores de los dos equipos y tambien las posiciones en las que juegan
             Estimate.Estimate(self.conexion,str(self.team1.currentText()),str(self.team2.currentText())) #creamos el nuevo objeto y ya se encarga de llamar a todos los métodos el solo
             
-    """    
-    #Para poner el background sobrescribimos el QWidget
-    def paintEvent(self, event):
-        painter = QPainter(self)
-        painter.drawPixmap(self.rect(), QPixmap("../resources/ball-soccer-600x400.jpg"))
-        QWidget.paintEvent(self, event)
-    """
-        
-        
         
 if __name__ == "__main__":
     #Conexión y consulta
-    conexion = Conexion.Neo4j("bolt://localhost:11006", "neo4j", "SIBI20")
+    conexion = Conexion.Neo4j("bolt://localhost:7687", "neo4j", "SIBI20")
     teams = conexion.query("MATCH (p)-[r:PLAYS]->(c) RETURN DISTINCT c.id")
     
     #Ventana
