a329caaf9865474c064d8beffbce334cbde1869a
Fernando García Diez <fer@fer-pc>
2021-04-02 22:38:11 +0200

    Avance con la interfaz

--- src/SAJEF.py
+++ src/SAJEF.py
@@ -3,8 +3,7 @@
 
 import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QComboBox,QPushButton,QGridLayout,QWidget,QLabel,QErrorMessage,QCheckBox,QVBoxLayout
-from PyQt5.QtCore import QSize
+from PyQt5.QtWidgets import QComboBox,QPushButton,QGridLayout,QWidget,QLabel,QErrorMessage,QCheckBox,QHBoxLayout
 from PyQt5.QtGui import QIcon
 
 import Conexion #archivo de la conexión con Neo4j para hacer consultas
@@ -14,30 +13,45 @@ class MainWindow(QWidget):
     
     def __init__(self,teams,conexion):#le pasamos la conexión al constructor para que lo pueda usar la clase estimate
         QWidget.__init__(self)
-        
+        self.checkBoxList = [] #lista de checkboxes de los jugadores
         self.teams = teams
         self.conexion = conexion
         
         self.gridLayout = QGridLayout(self)
         self.setLayout(self.gridLayout)
-        #self.setFixedSize(QSize(500, 300))    
+        self.hBoxSubLayoutDefensa = QHBoxLayout() #en el sublayout metemos el tipo y los jugadores de este tipo   
+        self.hBoxLayoutPorteria = QHBoxLayout() #en layout horizontal metemos el sublayout para que al meterlo al gridlayout cuente como metemos un solo widget y no 2
+        self.hBoxLayoutDefensa = QHBoxLayout()
+        self.hBoxLayoutCentro = QHBoxLayout()
+        self.hBoxLayoutAtaque = QHBoxLayout()
+
         self.setWindowTitle("SAJEF") 
         self.setWindowIcon(QIcon('../resources/iconmonstr-soccer-1-240.png'))
         
         self.team1 = QComboBox(self)
         self.team2 = QComboBox(self)
         self.team2.currentTextChanged.connect(self.on_combobox_changed)
-        self.elegir1 = QLabel("Elija su Equipo")
-        self.elegir2 = QLabel("Elija el Equipo Rival")
+        self.choose1 = QLabel("Elija su Equipo")
+        self.choose2 = QLabel("Elija el Equipo Rival")
+        self.porteria = QLabel("Portería")
+        self.defensa = QLabel("Defensa")
+        self.centro = QLabel("Centro del Campo")
+        self.delantera = QLabel("Ataque")
         self.acept = QPushButton("Aceptar",self)
         self.acept.clicked.connect(self.accept_button) #evento para manejar el click del boton aceptar
         
-        self.gridLayout.addWidget(self.elegir1,0,0)
-        self.gridLayout.addWidget(self.elegir2,0,2)
+        self.gridLayout.addWidget(self.choose1,0,0)
+        self.gridLayout.addWidget(self.choose2,0,2)
         self.gridLayout.addWidget(self.team1,1,0)
         self.gridLayout.addWidget(self.acept,1,1)
         self.gridLayout.addWidget(self.team2,1,2)
-        self.vBoxLayout = QVBoxLayout() #layout para poner los checkboxes de los jugadores para elegir
+        #self.gridLayout.addWidget(self.porteria,2,0)
+        #self.gridLayout.addWidget(self.defensa,3,0)
+        #self.gridLayout.addWidget(self.centro,4,0)
+        #self.gridLayout.addWidget(self.delantera,5,0)
+        self.hBoxLayoutDefensa.addWidget(self.defensa)
+        
+
         
         self.errorTeam = QErrorMessage()
         self.errorTeam.setFixedSize(250,150)
@@ -48,34 +62,39 @@ class MainWindow(QWidget):
             self.team2.addItem(item[2:-2])
             self.team1.setItemIcon(index, QIcon('../resources/iconmonstr-soccer-1-240.png'))
             self.team2.setItemIcon(index, QIcon('../resources/iconmonstr-soccer-1-240.png'))
-            
 
-        team = str(self.team2.currentText())
-        team = self.conexion.query("MATCH (p)-[r:PLAYS]->(c) WHERE c.id='{team}' RETURN DISTINCT p.name,r.teamPosition".format(team=team))
-        self.checkBoxList = []
-        
-        for player in team:#creamos una lista de comboBoxes de tamaño los jugadores de cada equipo
-            self.checkBox = QCheckBox(player)    
-            self.checkBoxList.append(self.checkBox)
-            self.vBoxLayout.addWidget(self.checkBox,stretch=1) #añadimos las checkboxes al layout
-
-        self.gridLayout.addLayout(self.vBoxLayout,2,1)
         
     def on_combobox_changed(self):
-        
         #eliminamos los widgets antiguos
         for checkBox in self.checkBoxList:
-            self.vBoxLayout.removeWidget(checkBox)
+            self.gridLayout.removeWidget(checkBox)#ojo poner el layout que este mas encima
         #buscmaos los jugadores del equipo
         team2 = str(self.team2.currentText())
         team2 = self.conexion.query("MATCH (p)-[r:PLAYS]->(c) WHERE c.id='{team}' RETURN DISTINCT p.name,r.teamPosition".format(team=team2))
         self.checkBoxList = []
         #añadimos los jugadores con los checkboxes
         for player in team2:#creamos una lista de comboBoxes de tamaño los jugadores de cada equipo
-            self.checkBox = QCheckBox(player)    
+            self.checkBox = QCheckBox(player)
             self.checkBoxList.append(self.checkBox)
-            self.vBoxLayout.addWidget(self.checkBox) #añadimos las checkboxes al layout
-    
+
+            if(player.split("'")[3] == "GK" or player.split("'")[3] == "SUB,GK" or player.split("'")[3] == "RES,GK"):#dependiendo de lo que sea lo metemos a un layout diferente(la \ sirve para espacar caracteres)
+                self.hBoxLayoutPorteria.addWidget(self.checkBox)#añadimos las checkboxes al layout
+                
+            if(player.split("'")[3] == "CB" or player.split("'")[3] == "SUB,CB" or player.split("'")[3] == "RES,CB" or player.split("'")[3] == "LCB" or player.split("'")[3] == "SUB,LCB" or player.split("'")[3] == "RES,LCB" or player.split("'")[3] == "RCB" or player.split("'")[3] == "SUB,RCB" or player.split("'")[3] == "RES,RCB" or player.split("'")[3] == "LB" or player.split("'")[3] == "SUB,LB" or player.split("'")[3] == "RES,LB"):
+                self.hBoxSubLayoutDefensa.addWidget(self.checkBox)
+            
+            if():
+                self.hBoxLayoutCentro.addWidget(self.checkBox)
+            
+            if():
+                self.hBoxLayoutAtaque.addWidget(self.checkBox)
+                
+        self.hBoxLayoutDefensa.addLayout(self.hBoxSubLayoutDefensa)
+        self.gridLayout.addLayout(self.hBoxLayoutDefensa,2,0,1,3)
+        #self.gridLayout.addLayout(self.hBoxLayoutPorteria,2,1,1,2) #el tercer número indica el número de filas que quiero juntar y el cuarto el número de columnas
+        #self.gridLayout.addLayout(self.hBoxLayoutDefensa,3,1,1,2)
+        #self.gridLayout.addLayout(self.hBoxLayoutCentro,4,1,1,2)
+        #self.gridLayout.addLayout(self.hBoxLayoutAtaque,5,1,1,2)
     def accept_button(self):
         if str(self.team1.currentText()) == str(self.team2.currentText()):#si los dos equipos son el mismo,mostramos un error
             self.errorTeam.showMessage("No puedes elegir el mismo equipo.")
